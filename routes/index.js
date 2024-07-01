662c9c42d7c176d6d5d1eb26acd8a2c9c4bea2d7
SamuelC1105 <s.j.clerke@hotmail.com>
2024-07-02 01:48:48 +0930

    styling in main.css for index.html and general cases

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/public/index.html');
+  res.redirect('public/index.html');
 });
 
 module.exports = router;
