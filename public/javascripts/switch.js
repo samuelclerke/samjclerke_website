0f088f1c9829844b300d0e43f93f268e66a6ffe9
SamuelC1105 <s.j.clerke@hotmail.com>
2025-01-26 23:08:57 +1030

    added images and more to about

---
+++ public/javascripts/switch.js
@@ -0,0 +1,12 @@
+const about_button = document.getElementById("about");
+const projects_button = document.getElementById("projects");
+const contact_button = document.getElementById("contact");
+
+const about_me_sect = document.getElementById("about_me");
+const curr_proj_sect = document.getElementById("curr_projects");
+const past_proj_sect = document.getElementById("past_projects");
+const contact_me_sect = document.getElementById("contact_me");
+
+about_button.addEventListener("click", ()=> {
+  
+})
